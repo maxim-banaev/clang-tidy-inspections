460b05a1e033dc180bb1c20d27cdcddfd1f8c65b
Maxim Banaev <Maxim.Banaev@jetbrains.com>
2018-05-30 10:01:53 +0300

    update project

---
+++ include/readability/readability-container-size-empty.h
@@ -0,0 +1,10 @@
+#ifndef CPP_12784_READABILITY_CONTAINER_SIZE_EMPTY_H
+#define CPP_12784_READABILITY_CONTAINER_SIZE_EMPTY_H
+
+namespace readability {
+    namespace container_size_empty {
+        void check();
+    } // namespace container_size_empty
+} // namespace readability
+
+#endif //CPP_12784_READABILITY_CONTAINER_SIZE_EMPTY_H
