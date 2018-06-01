8b3387175f4c5e421a1c4ec8b38787bef244c7bf
Maxim Banaev <Maxim.Banaev@jetbrains.com>
2018-06-01 16:50:10 +0300

    else after return

---
+++ include/readability/readability-else-after-return.h
@@ -0,0 +1,10 @@
+#ifndef CPP_12784_READABILITY_ELSE_AFTER_RETURN_H
+#define CPP_12784_READABILITY_ELSE_AFTER_RETURN_H
+
+namespace readability {
+    namespace else_after_return {
+        void check();
+    } // namespace else_after_return
+} // namespace readability
+
+#endif //CPP_12784_READABILITY_ELSE_AFTER_RETURN_H
