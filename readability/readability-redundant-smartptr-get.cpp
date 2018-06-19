a50395fcca3fda97233608be7947c200f26a4a9d
Maxim Banaev <Maxim.Banaev@jetbrains.com>
2018-06-19 18:03:00 +0300

    update redundant-smartptr-get

--- readability/readability-redundant-smartptr-get.cpp
+++ readability/readability-redundant-smartptr-get.cpp
@@ -2,9 +2,11 @@
 // google-readability-redundant-smartptr-get redirects here as an alias for this check.
 //
 // Find and remove redundant calls to smart pointer’s .get() method.
-//
+
+// https://clang.llvm.org/extra/clang-tidy/checks/readability-redundant-smartptr-get.html
 
 #include <iostream>
+#include <memory>
 
 #pragma clang diagnostic push
 #pragma ide diagnostic ignored "OCUnusedGlobalDeclarationInspection"
@@ -17,6 +19,7 @@ namespace readability {
         };
 
         void check() {
+            std::cout << "-- check readability-redundant-smartptr-get" << std::endl;
             std::shared_ptr <A> s_a;
             s_a.get()->func();
         }
