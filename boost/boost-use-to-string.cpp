c893bfdbc97c0b85ab7f973b041dab89c6ad86cc
Maxim Banaev <Maxim.Banaev@jetbrains.com>
2020-08-28 15:50:19 +0300

    Add boost-use-to-string

---
+++ boost/boost-use-to-string.cpp
@@ -0,0 +1,24 @@
+// boost-use-to-string
+// This check finds conversion from integer type like int to std::string or
+// std::wstring using boost::lexical_cast, and replace it with calls to
+// std::to_string and std::to_wstring.
+//
+// It doesn’t replace conversion from floating points despite the to_string
+// overloads, because it would change the behaviour.
+
+// https://clang.llvm.org/extra/clang-tidy/checks/boost-use-to-string.html
+
+
+#include <iostream>
+#include <boost/lexical_cast.hpp>
+
+namespace boost::use_to_string {
+void check() {
+  auto str = boost::lexical_cast<std::string>(42);
+  auto wstr = boost::lexical_cast<std::wstring>(2137LL);
+
+  std::cout << "-- check boost-use-to-string" << std::endl;
+
+
+}
+} // namespace boost::use_to_string
---
+++ include/boost/boost-use-to-string.h
@@ -0,0 +1,9 @@
+// NOLINTNEXTLINE(llvm-header-guard)
+#ifndef BOOST_USE_TO_STRING_H
+#define BOOST_USE_TO_STRING_H
+
+namespace boost::use_to_string {
+void check();
+} // namespace boost::use_to_string
+
+#endif // BOOST_USE_TO_STRING_H
